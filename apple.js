ca47d4637984dd75fbfa083b03c0f14623f87b87
yash <pyashraj23@gmail.com>
2022-10-19 19:42:04 +0530

    code

--- apple.js
+++ apple.js
@@ -12,3 +12,18 @@ function closecoupan(){
     }
     document.getElementById('navspace').style.opacity="1";
 }
+function Drklgt(){
+    document.body.classList.toggle('bcolor');
+    // document.getElementsByTagName('div')[38].classList.toggle('lgtfoot')
+    // document.getElementsByTagName('div')[39].classList.toggle('lgtfoot')
+    // document.getElementsByTagName('div')[40].classList.toggle('lgtfoot')
+    // document.getElementsByTagName('div')[41].classList.toggle('lgtfoot')
+    // document.getElementsByTagName('div')[42].classList.toggle('lgtfoot')
+    i=38;
+    while(i<=42){
+        document.getElementsByTagName('div')[i].classList.toggle('lgtfoot');
+        i++;
+    }
+    document.getElementsByTagName('p')[0].classList.toggle('redpdrk');
+    document.getElementsByTagName('button')[2].classList.toggle('cngbtnd');
+}
